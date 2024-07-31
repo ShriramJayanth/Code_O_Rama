9c3be88cfe86d4ef01e72680010e89e9dce40392
ManikavelP <manikavelp.cse2022@citchennai.net>
2024-07-31 11:39:55 +0530

    course1 added

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,7 +2,8 @@
 import Navbar from "@/components/Navbar";
 import {useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
-
+import Image from "next/image";
+import course1 from "../../assets/course1Img.jpeg"
 export default function Home() {
     const router=useRouter();
     const [message, setMessage] = useState('');
@@ -33,6 +34,36 @@ export default function Home() {
     return (
        <div>
         <Navbar home={true} name={message}/>
-       </div>
+        <div className="w-full  h-screen flex justify-center items-center">
+        <div className="w-[90%] h-[90%] flex flex-col justify-start">
+          <div className="w-full h-[15%] bg-red-200 flex flex-col justify-start p-5 gap-1.5">
+            <h1 className="font-bold text-4xl">Walk Through</h1>
+            <p className="text-gray-400 text-lg">
+              Stay on track—your security skills are growing!
+            </p>
+          </div>
+          <div className="w-full h-full bg-red-300 p-5 overflow-auto flex flex-row gap-16">
+            <div className="w-[30%] h-[63%]  rounded-2xl bg-white border-gray-400 border-double border-8 flex flex-col ">
+              <div className="w-full h-auto ">
+                <Image
+                  src={course1}
+                  className="object-cover rounded-lg"
+                  alt="Picture"
+                />
+              </div>
+              <div className="w-full h-full m-5 flex flex-col" >
+                <h1 className="font-bold text-lg">Password Power-up</h1>
+                <p className="text-gray-400 font-semibold ">Welcome to the Strong Passwords Walkthrough!</p>
+                
+              </div>
+            </div>
+
+            
+          </div>
+        </div>
+      </div>
+    </div>
+        
+  
     )
 }
