c67a661819ac563afb70cda5b24b6539410404c1
ShriramJayanth <csshriramjayanth.cse2022@citchennai.net>
2024-08-01 21:22:15 +0530

    title changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "BalaApp",
+  title: "PhishTank",
   description: "by team Revil",
 };
 
