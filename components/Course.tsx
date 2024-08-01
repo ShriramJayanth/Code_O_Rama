81edcf70f1b5136c4fe3fe0de2fc57a4e95d5f41
Bala <balarl.cs2022@citchennai.net>
2024-08-01 09:31:48 +0530

    2 more courses added

--- components/Course.tsx
+++ components/Course.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import course1 from "../assets/course1Img.jpeg";
+import course2 from "../assets/course2Img.jpg"
+import course3 from "../assets/course3Img.png"
 const Course = () => {
   return (
     <>
@@ -19,6 +21,37 @@ const Course = () => {
           </p>
         </div>
       </div>
+      <div className="w-[30%] h-[63%]  rounded-2xl bg-white border-gray-400 border-double border-8 flex flex-col ">
+        <div className="w-full h-auto ">
+          <Image
+            src={course2}
+            className="object-cover rounded-lg"
+            alt="Picture"
+          />
+        </div>
+        <div className="w-full h-full m-5 flex flex-col">
+          <h1 className="font-bold text-lg">Linux Terminal Essentials</h1>
+          <p className="text-gray-400 font-semibold ">
+            Mastering Basic Commands Course
+          </p>
+        </div>
+      </div>
+      <div className="w-[30%] h-[63%]  rounded-2xl bg-white border-gray-400 border-double border-8 flex flex-col ">
+        <div className="w-full h-auto ">
+          <Image
+            src={course3}
+            className="object-cover rounded-lg"
+            alt="Picture"
+          />
+        </div>
+        <div className="w-full h-full m-5 flex flex-col">
+          <h1 className="font-bold text-lg">Python Basics Bootcamp</h1>
+          <p className="text-gray-400 font-semibold ">
+            Beginner's Fast-Track Course
+          </p>
+        </div>
+      </div>
+      
     </>
   );
 };
